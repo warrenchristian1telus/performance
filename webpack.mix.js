54b767255a13dc0787c0bcf44de27e26e9f2a054
Kousthubha-M <74929858+Kousthubha-M@users.noreply.github.com>
2021-05-11 23:03:18 -0400

    Color Palette changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js').
-    postCss('resources/css/app.css', 'public/css', [
-                                                    require('postcss-import'),
-                                                    require('autoprefixer'),
+    sass('resources/sass/app.scss','public/css', {}, [
+        require('postcss-import'),
+        require('autoprefixer'),
     ]);

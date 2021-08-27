06020bc4941ebfa32657f63660070909047d3f7d
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2021-08-27 11:06:36 -0700

    Revert "Update switch.blade.php"
    This reverts commit ae5e899122037dbd18b958a701fae316360f289e.

--- resources/views/my-team/partials/switch.blade.php
+++
@@ -1,1 +0,0 @@
-<x-button style="link" class="excuse-btn" data-user-id="{{$row->id}}" data-user-name="{{$row->name}}" data-toggle="modal" data-target="#employee-excused-modal">{{ $yesOrNo }}</x-button>

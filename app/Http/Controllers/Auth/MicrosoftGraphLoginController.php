ab8971b67aca44adab30bf8b6f1bf97422cc34bb
James Poon <james.poon@telus.com>
2021-12-17 08:07:21 -0800

    Ticket#325 Azure AD Integration fixed -- use id_token from azure to get the GUID and samaccountname

--- app/Http/Controllers/Auth/MicrosoftGraphLoginController.php
+++ app/Http/Controllers/Auth/MicrosoftGraphLoginController.php
@@ -2,218 +2,222 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\User;
-use Microsoft\Graph\Graph;
-
-use Microsoft\Graph\Model;
-use Illuminate\Support\Str;
-use Illuminate\Http\Request;
-use App\MicrosoftGraph\TokenCache;
-use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
+use App\MicrosoftGraph\TokenCache;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use GuzzleHttp\Exception\ClientException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use Microsoft\Graph\Graph;
+use Microsoft\Graph\Model;
 
 class MicrosoftGraphLoginController extends Controller
 {
+
     //
-       //
-       public function signin()
-       {
-         // Initialize the OAuth client
-         $oauthClient = new \League\OAuth2\Client\Provider\GenericProvider([
-           'clientId'                => env('OAUTH_APP_ID'),
-           'clientSecret'            => env('OAUTH_APP_PASSWORD'),
-           'redirectUri'             => env('OAUTH_REDIRECT_URI'),
-           'urlAuthorize'            => env('OAUTH_AUTHORITY').env('OAUTH_AUTHORIZE_ENDPOINT'),
-           'urlAccessToken'          => env('OAUTH_AUTHORITY').env('OAUTH_TOKEN_ENDPOINT'),
-           'urlResourceOwnerDetails' => '',
-           'scopes'                  => env('OAUTH_SCOPES')
-         ]);
-     
-         $authUrl = $oauthClient->getAuthorizationUrl();
-   
-         // Save client state so we can validate in callback
-         session(['oauthState' => $oauthClient->getState()]);
-   
-         // Redirect to AAD signin page
-         return redirect()->away($authUrl);
-       }
-     
-       public function callback(Request $request)
-       {
-   
-         // Validate state
-         $expectedState = session('oauthState');
-         $request->session()->forget('oauthState');
-         $providedState = $request->query('state');
-     
-   
-   
-         if (!isset($expectedState)) {
-           // If there is no expected state in the session,
-           // do nothing and redirect to the home page.
-           return redirect('/');
-         }
-     
-         if (!isset($providedState) || $expectedState != $providedState) {
-           return redirect('/')
-             ->with('error', 'Invalid auth state')
-             ->with('errorDetail', 'The provided auth state did not match the expected value');
-         }
+    public function signin()
+    {
+        // Initialize the OAuth client
+        $oauthClient = new \League\OAuth2\Client\Provider\GenericProvider([
+            'clientId' => env('OAUTH_APP_ID'),
+            'clientSecret' => env('OAUTH_APP_PASSWORD'),
+            'redirectUri' => env('OAUTH_REDIRECT_URI'),
+            'urlAuthorize' => env('OAUTH_AUTHORITY') . env('OAUTH_AUTHORIZE_ENDPOINT'),
+            'urlAccessToken' => env('OAUTH_AUTHORITY') . env('OAUTH_TOKEN_ENDPOINT'),
+            'urlResourceOwnerDetails' => '',
+            'scopes' => env('OAUTH_SCOPES'),
+        ]);
 
-         // Authorization code should be in the "code" query param
-         $authCode = $request->query('code');
-         if (isset($authCode)) {
-           // Initialize the OAuth client
-           $oauthClient = new \League\OAuth2\Client\Provider\GenericProvider([
-             'clientId'                => env('OAUTH_APP_ID'),
-             'clientSecret'            => env('OAUTH_APP_PASSWORD'),
-             'redirectUri'             => env('OAUTH_REDIRECT_URI'),
-             'urlAuthorize'            => env('OAUTH_AUTHORITY').env('OAUTH_AUTHORIZE_ENDPOINT'),
-             'urlAccessToken'          => env('OAUTH_AUTHORITY').env('OAUTH_TOKEN_ENDPOINT'),
-             'urlResourceOwnerDetails' => '',
-             'scopes'                  => env('OAUTH_SCOPES')
-           ]);
-     
+        $authUrl = $oauthClient->getAuthorizationUrl();
 
-           try {
-             // Make the token request
-             $accessToken = $oauthClient->getAccessToken('authorization_code', [
-               'code' => $authCode
-             ]);
-             
-             // Get User Information '/me'
-             $graph = new Graph();
-             $graph->setAccessToken($accessToken->getToken());
-     
-             $user = $graph->createRequest('GET', '/me')
-               ->setReturnType(Model\User::class)
-               ->execute();
+        // Save client state so we can validate in callback
+        session(['oauthState' => $oauthClient->getState()]);
 
-             $profilePhoto = "";
-             try {
-               $stream = $graph->createRequest('GET', '/me/photos/96x96/$value')
-                 ->setReturnType(\Psr\Http\Message\StreamInterface::class)
-                 ->execute();
+        // Redirect to AAD signin page
+        return redirect()->away($authUrl);
+    }
 
-                if (isset($stream)) {
-                  $profilePhoto = $stream->getContents();
-                }
-  
-              } catch(ClientException $e) {
-                 if ($e->getResponse()->getStatusCode() == '404') {
-                   // No action 
-                 } else {
-                     dd('throw error if not 404');
-                 // throw ExceptionWrapper::wrapGuzzleBadResponseException($e);
-                 }
-              }
+    public function callback(Request $request)
+    {
 
-            $tokenCache = new TokenCache();
-            $tokenCache->storeTokens($accessToken, $user, $profilePhoto);
+        // Validate state
+        $expectedState = session('oauthState');
+        $request->session()->forget('oauthState');
+        $providedState = $request->query('state');
 
-            // To Retrieve "samaccountname" and "GUID"
-            $parsedToken = $this->parseToken($accessToken);
-            // find or create a new user 
-            $isUser = User::where('azure_id', $user->getId() )->first();
+        if (!isset($expectedState)) {
+            // If there is no expected state in the session,
+            // do nothing and redirect to the home page.
+            return redirect('/');
+        }
 
-            if ($isUser) {
-                if (!($isUser->hasRole('employee'))) {
-                    $isUser->assignRole('employee'); 
-                }
+        if (!isset($providedState) || $expectedState != $providedState) {
+            return redirect('/')
+                ->with('error', 'Invalid auth state')
+                ->with('errorDetail', 'The provided auth state did not match the expected value');
+        }
 
-                if (!($isUser->guid)) {
-                  if (array_key_exists('bcgovGUID', $parsedToken)) {
-                      $isUser->samaccountname = property_exists($parsedToken, 'samaccountname') ? $parsedToken->samaccountname : null;
-                      $isUser->guid = property_exists($parsedToken, 'bcgovGUID') ? $parsedToken->bcgovGUID : null;
-                      $isUser->save();
-                  }
-                }
- 
-                //Auth::login($isUser);
-                Auth::loginUsingId( $isUser->id );
+        // Authorization code should be in the "code" query param
+        $authCode = $request->query('code');
+        if (isset($authCode)) {
+            // Initialize the OAuth client
+            $oauthClient = new \League\OAuth2\Client\Provider\GenericProvider([
+                'clientId' => env('OAUTH_APP_ID'),
+                'clientSecret' => env('OAUTH_APP_PASSWORD'),
+                'redirectUri' => env('OAUTH_REDIRECT_URI'),
+                'urlAuthorize' => env('OAUTH_AUTHORITY') . env('OAUTH_AUTHORIZE_ENDPOINT'),
+                'urlAccessToken' => env('OAUTH_AUTHORITY') . env('OAUTH_TOKEN_ENDPOINT'),
+                'urlResourceOwnerDetails' => '',
+                'scopes' => env('OAUTH_SCOPES'),
+            ]);
 
-            } else {
-
-                $createUser = User::create([
-                    'name' => $user->getDisplayName(),
-                    'email' => $user->getMail(),
-                    'azure_id' => $user->getId(),
-                    'password' => Hash::make('WatchDog'),
-                    'samaccountname' => property_exists($parsedToken, 'samaccountname') ? $parsedToken->samaccountname : null,
-                    'guid' => property_exists($parsedToken, 'bcgovGUID') ? $parsedToken->bcgovGUID : null,
+            try {
+                // Make the token request
+                $accessToken = $oauthClient->getAccessToken('authorization_code', [
+                    'code' => $authCode,
                 ]);
 
-                // assign default role 'employee'
-                $createUser->assignRole('employee');
+                // Get User Information '/me'
+                $graph = new Graph();
+                $graph->setAccessToken($accessToken->getToken());
 
-                //Auth::login($createUser);
-                Auth::loginUsingId( $createUser->id );
+                $user = $graph->createRequest('GET', '/me')
+                    ->setReturnType(Model\User::class)
+                    ->execute();
 
-                $request->session()->regenerate();
+                $profilePhoto = "";
+                try {
+                    $stream = $graph->createRequest('GET', '/me/photos/96x96/$value')
+                        ->setReturnType(\Psr\Http\Message\StreamInterface::class)
+                        ->execute();
 
+                    if (isset($stream)) {
+                        $profilePhoto = $stream->getContents();
+                    }
+
+                } catch (ClientException $e) {
+                    if ($e->getResponse()->getStatusCode() == '404') {
+                        // No action
+                    } else {
+                        dd('throw error if not 404');
+                        // throw ExceptionWrapper::wrapGuzzleBadResponseException($e);
+                    }
+                }
+
+                $tokenCache = new TokenCache();
+                $tokenCache->storeTokens($accessToken, $user, $profilePhoto);
+
+                // To Retrieve "samaccountname" and "GUID" from "id_token"
+                $parsedToken = $this->parseToken($accessToken->getValues()['id_token']);
+
+                // find or create a new user
+                $isUser = User::where('azure_id', $user->getId())->first();
+
+                if ($isUser) {
+                    if (!($isUser->hasRole('employee'))) {
+                        $isUser->assignRole('employee');
+                    }
+
+                    if (!($isUser->guid)) {
+                        if (property_exists($parsedToken, 'bcgovGUID')) {
+                            $isUser->samaccountname = property_exists($parsedToken, 'samaccountname') ? $parsedToken->samaccountname : null;
+                            $isUser->guid = property_exists($parsedToken, 'bcgovGUID') ? $parsedToken->bcgovGUID : null;
+                            $isUser->save();
+                        }
+                    }
+
+                    //Auth::login($isUser);
+                    Auth::loginUsingId($isUser->id);
+
+                } else {
+
+                    $createUser = User::create([
+                        'name' => $user->getDisplayName(),
+                        'email' => $user->getMail(),
+                        'azure_id' => $user->getId(),
+                        'password' => Hash::make('WatchDog'),
+                        'samaccountname' => property_exists($parsedToken, 'samaccountname') ? $parsedToken->samaccountname : null,
+                        'guid' => property_exists($parsedToken, 'bcgovGUID') ? $parsedToken->bcgovGUID : null,
+                    ]);
+
+                    // assign default role 'employee'
+                    $createUser->assignRole('employee');
+
+                    //Auth::login($createUser);
+                    Auth::loginUsingId($createUser->id);
+
+                    $request->session()->regenerate();
+
+                }
+
+                // /* the following code for debugging when no GUID returned from Token */
+                // if (env('APP_DEBUG')) {
+                //     if (!(property_exists($parsedToken, 'bcgovGUID'))) {
+                //         $dd_values['id_token'] = $accessToken->getValues()['id_token'];
+                //         $dd_values['parsedToken'] = $parsedToken;
+                //         dd($dd_values);
+                //     }
+                // }
+
+                return redirect()->intended(RouteServiceProvider::HOME);
+                //return redirect('/dashboard');
+
+            } catch (League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+
+                Log::error($e);
+
+                return redirect('/')
+                //  ->with('error', 'Error requesting access token')
+                //  ->with('errorDetail', $e->getMessage());
+                ->withErrors([
+                    'error' => 'Error requesting access token',
+                    'errorDetail' => $e->getMessage(),
+                ]);
             }
+        }
 
-            return redirect()->intended(RouteServiceProvider::HOME);
-            //return redirect('/dashboard');
+        Log::error('[Error on function callback in MicrosoftGraphLoginController.php --Empty Authorization code] ' .
+            $request->query('error_description'));
 
-           }
+        return redirect('/')
+            ->withErrors([
+                'error' => $request->query('error'),
+                'errorDetail' => $request->query('error_description'),
+            ]);
+    }
 
-           catch (League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
-
-             Log::error( $e );
-
-             return redirect('/')
-              //  ->with('error', 'Error requesting access token')
-              //  ->with('errorDetail', $e->getMessage());
-              ->withErrors([
-                'error' => 'Error requesting access token',
-                'errorDetail' =>  $e->getMessage(),
-              ]);
-           }
-         }
-
-         Log::error( '[Error on function callback in MicrosoftGraphLoginController.php --Empty Authorization code] ' .
-                   $request->query('error_description') );
-                 
-         return redirect('/')
-              ->withErrors([
-               'error' => $request->query('error'),
-               'errorDetail' => $request->query('error_description'),
-           ]);
-       }
-     
-       // <SignOutSnippet>
-       public function destroy(Request $request)
-       {
-          // Determine whether signon Azure or local database
-          if (empty(session('accessToken'))) {
+    // <SignOutSnippet>
+    public function destroy(Request $request)
+    {
+        // Determine whether signon Azure or local database
+        if (empty(session('accessToken'))) {
             $back_url = ('/');
-          } else {
-            $back_url = env('OAUTH_AUTHORITY')."/oauth2/v2.0/logout?post_logout_redirect_uri=http%3A%2F%2Flocalhost%3A8000%2F";
-          }
-  
-         $tokenCache = new TokenCache();
-         $tokenCache->clearTokens();
-   
-         // same as AuthenticatedSessionController::logout();
-         Auth::guard('web')->logout();
-         $request->session()->invalidate();
-         $request->session()->regenerateToken();
-   
+        } else {
+            $back_url = env('OAUTH_AUTHORITY') . "/oauth2/v2.0/logout?post_logout_redirect_uri=http%3A%2F%2Flocalhost%3A8000%2F";
+        }
 
-         return redirect( $back_url );
-   
-         // return redirect('/');
-       }
-       // </SignOutSnippet>
+        $tokenCache = new TokenCache();
+        $tokenCache->clearTokens();
 
-      private function parseToken ($token) {
+        // same as AuthenticatedSessionController::logout();
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect($back_url);
+
+        // return redirect('/');
+    }
+    // </SignOutSnippet>
+
+    private function parseToken($token)
+    {
         $base64Data = explode(".", $token)[1];
         return json_decode(base64_decode($base64Data));
     }
+
 }

dcc193cb3139f9c336e85e963364241e9498f06c
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2022-02-09 09:51:15 -0800

    Update StoreODSData.php

--- app/Console/Commands/StoreODSData.php
+++ app/Console/Commands/StoreODSData.php
@@ -2,10 +2,13 @@
 
 namespace App\Console\Commands;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use App\Models\EmployeeDemo;
+use App\Models\JobSchedAudit;
+use Carbon\Carbon;
 
 class StoreODSData extends Command
 {
@@ -40,37 +43,67 @@ class StoreODSData extends Command
      */
     public function handle()
     {
+      $start_time = Carbon::now();
+      $job_name = 'command:getDemoData';
+      // $audit_id = DB::table('job_sched_audit')->insertGetId(
+      $audit_id = JobSchedAudit::insertGetId(
+        [
+          'job_name' => $job_name,
+          'start_time' => date('Y-m-d H:i:s', strtotime($start_time)),
+          'status' => 'Initiated'
+        ]
+      );
+
         $demodata = Http::withHeaders(['Content-Type' => 'application/x-www-form-urlencoded'])->withBasicAuth(env('ODS_DEMO_CLIENT_ID'),env('ODS_DEMO_CLIENT_SECRET'))->get(env('ODS_DEMO_URI_1'));
+
+        $cutoff_time = Carbon::now();
 
         $data = $demodata['value'];
 
         foreach($data as $item){
-          EmployeeDemo::updateOrCreate([
-            'guid' => $item['GUID'],
-          ],[
-            'employee_id' => $item['employee_id'],
-            'empl_record' => $item['Empl_Record'],
-            'employee_first_name' => $item['employee_first_name'],
-            'employee_last_name' => $item['employee_last_name'],
-            'employee_status' => $item['employee_status'],
-            'employee_email' => $item['employee_email'],
-            'classification' => $item['classification'],
-            'deptid' => $item['deptid'],
-            'jobcode' => $item['Jobcode'],
-            'job_title' => $item['job_title'],
-            'position_number' => $item['Position_number'],
-            'position_start_date' => date('Y-m-d H:i:s', strtotime($item['position_start_date'])),
-            'manager_id' => $item['manager_id'],
-            'manager_first_name' => $item['manager_first_name'],
-            'manager_last_name' => $item['manager_last_name'],
-            'guid' => $item['GUID'],
-            'date_posted' => date('Y-m-d H:i:s', strtotime($item['date_posted'])),
-            'date_deleted' => date('Y-m-d H:i:s', strtotime($item['date_deleted'])),
-            'date_updated' => date('Y-m-d H:i:s', strtotime($item['date_updated'])),
-            'date_created' => date('Y-m-d H:i:s', strtotime($item['date_created'])),
-          ]);
+          DB::table('employee_demo')->updateOrInsert(
+            [
+              'guid' => $item['GUID'],
+            ],
+            [
+              'guid' => $item['GUID'],
+              'employee_id' => $item['employee_id'],
+              'empl_record' => $item['Empl_Record'],
+              'employee_first_name' => $item['employee_first_name'],
+              'employee_last_name' => $item['employee_last_name'],
+              'employee_status' => $item['employee_status'],
+              'employee_email' => $item['employee_email'],
+              'classification' => $item['classification'],
+              'deptid' => $item['deptid'],
+              'jobcode' => $item['Jobcode'],
+              'job_title' => $item['job_title'],
+              'position_number' => $item['Position_number'],
+              'position_start_date' => date('Y-m-d H:i:s', strtotime($item['position_start_date'])),
+              'manager_id' => $item['manager_id'],
+              'manager_first_name' => $item['manager_first_name'],
+              'manager_last_name' => $item['manager_last_name'],
+              'guid' => $item['GUID'],
+              'date_posted' => date('Y-m-d H:i:s', strtotime($item['date_posted'])),
+              'date_deleted' => date('Y-m-d H:i:s', strtotime($item['date_deleted'])),
+              'date_updated' => date('Y-m-d H:i:s', strtotime($item['date_updated'])),
+              'date_created' => date('Y-m-d H:i:s', strtotime($item['date_created'])),
+            ]
+          );
         };
 
-        //return 0;
+        $end_time = Carbon::now();
+        DB::table('job_sched_audit')->updateOrInsert(
+          [
+            'id' => $audit_id
+          ],
+          [
+            'job_name' => $job_name,
+            'start_time' => date('Y-m-d H:i:s', strtotime($start_time)),
+            'end_time' => date('Y-m-d H:i:s', strtotime($end_time)),
+            'cutoff_time' => date('Y-m-d H:i:s', strtotime($cutoff_time)),
+            'status' => 'Completed'
+          ]
+        );
+
     }
 }

68e6028dd51cde5ca6a68e75ec4622cc8743812f
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2022-02-28 11:53:39 -0800

    Fix Employee Demo Table
    Fix Employee Demo Table

--- database/migrations/2022_02_28_143910_alter_employee_demo_table_2.php
+++ database/migrations/2022_02_28_143910_alter_employee_demo_table_2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterEmployeeDemoTable extends Migration
+class AlterEmployeeDemoTable2 extends Migration
 {
     /**
      * Run the migrations.

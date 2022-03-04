d4e966913ce90eab9744767b0633e2b803372d7c
telusdcinco <86343751+telusdcinco@users.noreply.github.com>
2022-03-04 14:23:21 -0800

    Create 20220304_100000_drop_field_employee_demo_table.php

---
+++ database/migrations/20220304_100000_drop_field_employee_demo_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropFieldEmployeeDemoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('employee_demo', function (Blueprint $table) {
+            $table->dropColumn('estimated_years_service');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    /*public function down()
+    {
+        Schema::table('employee_demo', function (Blueprint $table) {
+            $table->dropColumn('organization');
+        });
+    }*/
+}

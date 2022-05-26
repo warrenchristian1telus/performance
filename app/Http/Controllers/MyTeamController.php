5a2dffab8e482646a0412d8acdce8a17e6e4c08e
jizhaogit <ji.zhao@telus.com>
2022-05-26 16:38:39 -0400

    My Team>Team Members tag error
     My Team>Team Members tag error

--- app/Http/Controllers/MyTeamController.php
+++ app/Http/Controllers/MyTeamController.php
@@ -31,6 +31,7 @@ class MyTeamController extends Controller
      */
     public function myEmployees(MyEmployeesDataTable $myEmployeesDataTable, SharedEmployeeDataTable $sharedEmployeeDataTable)
     {
+        $tags = Tag::all()->toArray();
         $goaltypes = GoalType::all();
         $eReasons = ExcusedReason::all();
         $conversationTopics = ConversationTopic::all();
@@ -47,7 +48,7 @@ class MyTeamController extends Controller
         $showSignoff = false;
         $myEmpTable = $myEmployeesDataTable->html();
         $sharedEmpTable = $sharedEmployeeDataTable->html();
-        return view('my-team/my-employees',compact('goals', 'employees', 'goaltypes', 'conversationTopics', 'type', 'myEmpTable', 'sharedEmpTable', 'eReasons', 'showSignoff'));
+        return view('my-team/my-employees',compact('goals', 'tags', 'employees', 'goaltypes', 'conversationTopics', 'type', 'myEmpTable', 'sharedEmpTable', 'eReasons', 'showSignoff'));
         // return $myEmployeesDataTable->render('my-team/my-employees',compact('goals', 'employees', 'goaltypes', 'conversationTopics', 'participants', 'type'));
     }
 

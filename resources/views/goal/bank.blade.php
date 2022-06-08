bbad304242366549bd111478ed458fd0e85abb00
jizhaogit <ji.zhao@telus.com>
2022-06-08 18:25:58 -0400

    #461 #489
    Conversation issues fixing

--- resources/views/goal/bank.blade.php
+++ resources/views/goal/bank.blade.php
@@ -107,7 +107,7 @@
                 </div>
             </div>
         </form>
-        @if(Auth::user()->hasSupervisorRole())
+        @if(Auth::user()->hasRole('Supervisor'))
         @php $shareWithLabel = 'Audience' @endphp
         @php $doNotShowInfo = true @endphp
         <div>
